ddd8614abd3c407820cf45e29ac589ae1ce3cca8
Jasmine Ryon <jryon12@priorypride.net>
2021-05-08 14:14:26 -0700

    added flash error to helper, redirect_if_not_logged_in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -49,9 +49,10 @@ class ApplicationController < Sinatra::Base
 
     def redirect_if_not_logged_in
       if !logged_in?
+        flash[:not_logged_in] = "You need to be logged in first, in order to view what you would like to see."
         redirect "/login"
       end
     end
-    
+
   end
 end
