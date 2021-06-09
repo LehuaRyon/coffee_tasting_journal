80eff9c85b0bc292ed260183aa5e5a2fb92e1b0c
Jasmine Ryon <jryon12@priorypride.net>
2021-06-09 16:08:30 -0700

    updated post signup do

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -6,7 +6,8 @@ class UserController < ApplicationController
 
     post '/signup' do
         user = User.new(params)
-        if user.username.blank? || user.email.blank? || user.password.blank? || user.first_name.blank? || user.last_name.blank? || User.find_by_email(params[:email]) || User.find_by_username(params[:username])
+        if !user.save
+        # if user.username.blank? || user.email.blank? || user.password.blank? || user.first_name.blank? || user.last_name.blank? || User.find_by_email(params[:email]) || User.find_by_username(params[:username])
             flash[:signup_error] = "Invalid sign up, try again. Please fill out all the fields."
             redirect '/signup'
         else 
